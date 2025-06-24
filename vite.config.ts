79c42555131258fe7c0dcda25e7a68106b10fba7
Benrgy <36407295+Benrgy@users.noreply.github.com>
2025-06-24 20:06:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/cloudways-savings-genius/",
   server: {
     host: "::",
     port: 8080,
@@ -20,4 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/' // Since this will be the base directory
 }));
